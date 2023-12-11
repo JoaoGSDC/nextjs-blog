a2c2294a6824c4e70fb78effb0f9abaf410a8e3d
JoaoGSDC <joaogsdc@gmail.com>
2023-12-11 18:04:39 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,131 +1,53 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+"use client";
+
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [urlInput, setUrlInput] = useState("");
+  const [data, setData] = useState();
+
+  /* const axiosClient = axios.create({
+    withCredentials: true,
+    baseURL: "https://dev.api.haras.pro/api/",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjgxYTBhMmVjLTZiOWEtNGFmYi1iYjEzLWZkYjAyY2U5ZDJiMiIsImVtYWlsIjoidmluaWNpdXNAY29uZXhwYXkuY29tLmJyIiwiVXNlck5hbWUiOiJ2aW5pY2l1cyIsIlVzZXJJbWFnZSI6InVuZGVmaW5lZCIsInJvbGVzIjoiU3lzdGVtIiwibmJmIjoxNzAxOTc1OTAzLCJleHAiOjE3MDIwNjIzMDMsImlhdCI6MTcwMTk3NTkwMywiaXNzIjoiSXNzdWVyIiwiYXVkIjoiQXVkaWVuY2UifQ.Q3-of0eOxPDwtDmp0hWKtb5-nScdxDRBYIrVggd5V3g",
+    },
+  });
+
+  const teste = useCallback(async () => {
+    const response = await axiosClient.get("Farms");
+    console.log(response);
+  }, []);
+
+  useEffect(() => {
+    teste();
+  }, [teste]); */
+
+  const testando = async () => {
+    const response = await axios.get(urlInput, {
+      withCredentials: true,
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjgxYTBhMmVjLTZiOWEtNGFmYi1iYjEzLWZkYjAyY2U5ZDJiMiIsImVtYWlsIjoidmluaWNpdXNAY29uZXhwYXkuY29tLmJyIiwiVXNlck5hbWUiOiJ2aW5pY2l1cyIsIlVzZXJJbWFnZSI6InVuZGVmaW5lZCIsInJvbGVzIjoiU3lzdGVtIiwibmJmIjoxNzAyMzA4NzUzLCJleHAiOjE3MDIzOTUxNTMsImlhdCI6MTcwMjMwODc1MywiaXNzIjoiSXNzdWVyIiwiYXVkIjoiQXVkaWVuY2UifQ.oupP6qD_fG9AYmxo6XVxIllBEOyk9eol_vjWHzdqHzs",
+      },
+    });
+    setData(response.data);
+  };
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <label>Digite a URL:</label>
+      <input value={urlInput} onChange={(e) => setUrlInput(e.target.value)} />
+      <button type="button" onClick={() => testando()}>
+        Testar
+      </button>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+      <h1>Dados carregados:</h1>
+      <h3>{JSON.stringify(data)}</h3>
+    </>
   );
 }
